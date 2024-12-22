5c4bf425745746d9575e29bb6a3cc877d6e8bf5a
Major Sweet <majorswe@un-k6.kzn.21-school.ru>
2024-12-22 21:44:26 +0300

    Sravnitel fix

--- main.c
+++ main.c
@@ -1,19 +1,21 @@
 #include "./s21_decimal.h"
 
 int main() {
-
-  s21_decimal value_1 = {12, 0, 0, 1 << 31};
-  s21_decimal value_2 = {13, 0, 0, 1 << 31};
+  s21_decimal value_1 = {5, 0, 0, 1<<31};
+  s21_decimal value_2 = {7, 0, 0, 0};
   s21_decimal result;
   print_bin_decimal(value_1);
   printf("\n");
-  //print_bin_decimal(value_2);
-  //printf("\n");mal res = s21_sub(value_1, value_2);
-  //print_bin_decimal(res);
-
- // value_1 = decimal_shift_left(value_1, 1);
-  print_bin_decimal(value_1);
-  printf("%d\n", s21_is_less(value_1, value_2));
-
+  print_bin_decimal(value_2);
+  printf("\n"); 
+  s21_decimal res = s21_sub(value_1, value_2);
+  print_bin_decimal(res);
+  
+  printf("< %d\n", s21_is_less(value_1, value_2));
+  printf("<= %d\n", s21_is_less_or_equal(value_1, value_2));
+  printf("> %d\n", s21_is_greater(value_1, value_2));
+  printf(">= %d\n", s21_is_greater_or_equal(value_1, value_2));
+  printf("== %d\n", s21_is_equal(value_1, value_2));
+  printf("!= %d\n", s21_is_not_equal(value_1, value_2));
   return 0;
 }
