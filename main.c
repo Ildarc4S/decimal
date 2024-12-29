620cbb87a02e86d1281cfa72c736a83695c5e8d9
Ildarc4S <ildarc4S@gmail.com>
2024-12-29 19:47:36 +0300

    mul in process

--- main.c
+++ main.c
@@ -1,9 +1,8 @@
 #include "./s21_decimal.h"
 
 int main() {
-
-  s21_decimal value_1 = {0, 0, 13, (1 << 31)};
-  s21_decimal value_2 = {0, 0, 14, 1 << 31};
+  s21_decimal value_1 = {{0, 0, 13, (1 << 31)}};
+  s21_decimal value_2 = {{0, 0, 14, 1 << 31}};
   s21_decimal result;
   print_bin_decimal(value_1);
   printf("\n");
@@ -12,7 +11,7 @@ int main() {
   s21_decimal res = s21_sub(value_1, value_2);
   print_bin_decimal(res);
 
-
+  printf("(%d)", s21_mul(10,20));
   print_bin(1 << 1);
   return 0;
 }
