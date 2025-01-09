105c6e393a0f5661a7f252f84531b2fe4592ae71
Ildarc4S <ildarc4S@gmail.com>
2025-01-09 17:01:36 +0300

    Fix add and mul

--- arithmetic.c
+++ arithmetic.c
@@ -2,14 +2,20 @@
 
 void s21_binary_add(s21_big_decimal num_one, s21_big_decimal num_two, s21_big_decimal* result) {
   s21_big_decimal temp_decimal = num_one; 
+  int decimal_is_null = 1;
   while (!is_null(temp_decimal)) {
+    decimal_is_null = 0;
     s21_bin_and(num_one, num_two, &temp_decimal);
     s21_bin_shift_left_one(&temp_decimal);
 
     s21_bin_xor(num_one, num_two, &num_one);
     num_two = temp_decimal;
   }
-  *result = num_one;
+  if (decimal_is_null) {
+    *result = num_two;
+  } else {
+    *result = num_one;
+  }
 }
 
 void s21_binary_sub(s21_big_decimal num_one, s21_big_decimal num_two, s21_big_decimal* result) {
@@ -29,9 +35,8 @@ void s21_binary_sub(s21_big_decimal num_one, s21_big_decimal num_two, s21_big_de
 void s21_binary_mul(s21_big_decimal num_one, s21_big_decimal num_two, s21_big_decimal* result) {
   s21_big_decimal temp = num_one;
   int max_bit_index = get_max_bit(num_two);
-  printf("max:%d\n", max_bit_index);
   for (int i = 0; i <= max_bit_index; i++) {
-    if (is_set_bit(num_two.bits[max_bit_index/31], max_bit_index % 31)) {
+    if (is_set_bit(num_two.bits[i/32], i % 32)) {
       s21_binary_add(*result, temp, result);    
     }
     s21_bin_shift_left_one(&temp);
