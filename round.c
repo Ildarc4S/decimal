9fc279cea30ba66aae48c72ea56f636a29b0c983
Ildarc4S <ildarc4S@gmail.com>
2025-01-11 23:56:38 +0300

    Add round bancking define

--- round.c
+++ round.c
@@ -28,6 +28,7 @@ int s21_floor(s21_decimal value, s21_decimal *result) {
 }
 
 int s21_round(s21_decimal value, s21_decimal *result) { }
+void s21_banck_round(s21_decimal value, s21_decimal *result) { }
 
 int s21_truncate(s21_decimal value, s21_decimal *result) {
   s21_big_decimal big_value;
--- round.h
+++ round.h
@@ -8,6 +8,7 @@
 
 int s21_floor(s21_decimal value, s21_decimal *result);
 int s21_round(s21_decimal value, s21_decimal *result);
+void s21_banck_round(s21_decimal value, s21_decimal *result);
 int s21_truncate(s21_decimal value, s21_decimal *result);
 
 #endif
