3195598c9edf3680c8fb41bbd26a8f45e959fc73
Ildarc4S <ildarc4S@gmail.com>
2025-01-17 12:28:05 +0300

    Good work, fix overflow in add

--- src/arithmetic.c
+++ src/arithmetic.c
@@ -20,12 +20,19 @@ int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal* result) {
   if (sign_one == POSITIVE && sign_two == POSITIVE) {
     result_code = s21_add_util(value_1, value_2, result);
   } else if (sign_one == NEGATIVE && sign_two == NEGATIVE) {
+    printf("Negate");
+    s21_set_sign(&value_1, 0);
+    s21_set_sign(&value_2, 0);
     result_code = s21_add_util(value_1, value_2, result);
+    s21_set_sign(result, 1);
+    if (result_code == kCodeBig) {
+        result_code = kCodeSmall;
+    }
   } else if (sign_one == POSITIVE && sign_two == NEGATIVE) {
     /*printf("PN\n");*/
     s21_set_sign(&value_2, 0);
     /*printf("Val2:\n");*/
-    /*s21_print_bin_decimal(value_2);*/
+    /*//s21_print_bin_decimal(value_2);*/
     if (s21_is_greater_or_equal(value_1, value_2)) { // 4 - 2 = sub(4,2) 
       /*printf("YES, great\n");*/
       result_code = s21_sub_util(value_1, value_2, result);
@@ -37,7 +44,7 @@ int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal* result) {
     s21_set_sign(&value_1, 0);
     if (s21_is_greater_or_equal(value_1, value_2)) {
       result_code = s21_sub_util(value_1, value_2, result);
-      s21_set_decimal_scale(result, 1);
+      s21_set_sign(result, 1);
     } else {
       result_code = s21_sub_util(value_2, value_1, result);
     }
@@ -115,44 +122,46 @@ int s21_add_util(s21_decimal value_1, s21_decimal value_2,
   /*s21_print_bin_big_decimal(big_value_2);*/
 
   s21_binary_add(big_value_1, big_value_2, &big_result);
-  s21_print_bin_big_decimal(big_result);
+  //s21_print_bin_big_decimal(big_result);
 
   s21_big_decimal temp = big_result;
+  s21_big_decimal trunc_temp = big_result;
   s21_div_to_ten(&temp);
   int scale = s21_get_big_decimal_scale(big_result);
   printf("Scale:%d\n", scale); 
   while(s21_get_max_bit(temp) > 96 && scale > 0) {
     s21_div_to_ten(&temp);
     s21_div_to_ten(&big_result);
-    s21_print_bin_big_decimal(big_result);
+    //s21_print_bin_big_decimal(big_result);
     scale--;
   }
-  s21_print_bin_big_decimal(big_result);
+  //s21_print_bin_big_decimal(big_result);
   s21_set_scale(&big_result, scale);
   if (s21_get_max_bit(big_result) >= 96 && scale > 0) {
     printf("Okrug");
     s21_big_decimal big_truncate_decimal = big_result;
     /*printf("Big_trunc_dec\n");*/
-    s21_print_bin_big_decimal(big_truncate_decimal);
+    //s21_print_bin_big_decimal(big_truncate_decimal);
 
     s21_big_decimal remaind;
     s21_null_big_decimal(&remaind);
 
     s21_big_decimal_truncate(&big_truncate_decimal);
-    s21_print_bin_big_decimal(big_truncate_decimal);
+    //s21_print_bin_big_decimal(big_truncate_decimal);
 
-    s21_normalization(&big_result, &big_truncate_decimal);
-    s21_binary_sub(big_result, big_truncate_decimal, &remaind); 
+    s21_normalization(&trunc_temp, &big_truncate_decimal);
+    s21_binary_sub(trunc_temp, big_truncate_decimal, &remaind); 
   
-    s21_print_bin_big_decimal(big_result);
+    //s21_print_bin_big_decimal(big_result);
     s21_div_to_ten(&big_result);
-    s21_print_bin_big_decimal(big_result);
+    //s21_print_bin_big_decimal(big_result);
 
     scale--;
     s21_set_scale(&big_result, scale);
     s21_banck_round(&big_result, remaind);
   }
-  s21_print_bin_big_decimal(big_result);
+  //s21_print_bin_big_decimal(big_result);
+  printf("Max:%d\n", s21_get_max_bit(big_result));
   if (s21_get_max_bit(big_result) >= 96) {
     printf("BIG\n");
     result_code = kCodeBig;
@@ -178,7 +187,7 @@ int s21_sub_util(s21_decimal value_1, s21_decimal value_2,
   /*s21_print_bin_big_decimal(big_value_2);*/
 
   s21_binary_sub(big_value_1, big_value_2, &big_result);
-  s21_print_bin_big_decimal(big_result);
+  //s21_print_bin_big_decimal(big_result);
 
   s21_big_decimal temp = big_result;
   s21_big_decimal trunc_temp = big_result;
@@ -190,35 +199,35 @@ int s21_sub_util(s21_decimal value_1, s21_decimal value_2,
     s21_div_to_ten(&temp);
     printf("%d\n", s21_get_max_bit(temp));
     s21_div_to_ten(&big_result);
-    s21_print_bin_big_decimal(big_result);
+    //s21_print_bin_big_decimal(big_result);
     scale--;
   }
-  s21_print_bin_big_decimal(big_result);
+  //s21_print_bin_big_decimal(big_result);
   s21_set_scale(&big_result, scale);
   if (s21_get_max_bit(big_result) >= 96 && scale > 0) {
     printf("Okrug");
     s21_big_decimal big_truncate_decimal = big_result;
     /*printf("Big_trunc_dec\n");*/
-    s21_print_bin_big_decimal(big_truncate_decimal);
+    //s21_print_bin_big_decimal(big_truncate_decimal);
 
     s21_big_decimal remaind;
     s21_null_big_decimal(&remaind);
 
     s21_big_decimal_truncate(&big_truncate_decimal);
-    s21_print_bin_big_decimal(big_truncate_decimal);
+    //s21_print_bin_big_decimal(big_truncate_decimal);
 
     s21_normalization(&trunc_temp, &big_truncate_decimal);
     s21_binary_sub(trunc_temp, big_truncate_decimal, &remaind); 
   
-    s21_print_bin_big_decimal(big_result);
+    //s21_print_bin_big_decimal(big_result);
     s21_div_to_ten(&big_result);
-    s21_print_bin_big_decimal(big_result);
+    //s21_print_bin_big_decimal(big_result);
 
     scale--;
     s21_set_scale(&big_result, scale);
     s21_banck_round(&big_result, remaind);
   }
-  s21_print_bin_big_decimal(big_result);
+  //s21_print_bin_big_decimal(big_result);
   if (s21_get_max_bit(big_result) >= 96) {
     printf("BIG\n");
     result_code = kCodeBig;
