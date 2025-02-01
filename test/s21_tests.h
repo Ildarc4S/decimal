a724cfbcb5b4dfb567c9f7f41b0973bc3118299a
Ildarc4S <ildarc4S@gmail.com>
2025-02-01 09:29:39 +0300

    Add tests

--- test/s21_tests.h
+++ test/s21_tests.h
@@ -21,6 +21,11 @@ void test_is_less_or_equal(s21_decimal decimal1, s21_decimal decimal2, int check
 void test_is_greater(s21_decimal decimal1, s21_decimal decimal2, int check);
 void test_is_not_equal(s21_decimal decimal1, s21_decimal decimal2, int check);
 
+void test_from_decimal_to_float(s21_decimal decimal, float check);
+void test_from_decimal_to_int(s21_decimal decimal, int check);
+void test_from_float_to_decimal(float num, s21_decimal decimal_check);
+void test_from_int_to_decimal(int number, s21_decimal decimal_check);
+
 Suite *add_suite(void);
 Suite *sub_suite(void);
 Suite *div_suite(void);
@@ -38,6 +43,11 @@ Suite *round_suite(void);
 Suite *floor_suite(void);
 Suite *negate_suite(void);
 
+Suite *from_int_to_decimal_suite(void);
+Suite *from_float_to_decimal_suite(void);
+Suite *from_decimal_to_int_suite(void);
+Suite *from_decimal_to_float_suite(void);
+
 #define TEST_OTHER_OK 0
 #define TEST_OTHER_ERROR 1
 
@@ -48,3 +58,6 @@ Suite *negate_suite(void);
 
 #define TEST_COMPARISON_FALSE 0
 #define TEST_COMPARISON_TRUE 1
+
+#define TEST_CONVERSION_OK 0
+#define TEST_CONVERSION_ERROR 1
